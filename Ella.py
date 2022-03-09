b21919c7e66eb19bac4274db255e8ae48c597eee
Hunu Emmanuella <100598015+Ellababe@users.noreply.github.com>
2022-03-09 12:00:56 +0000

    Add files via upload
    My first trial and i'm so happy. Cheers to many more

---
+++ Ella.py
@@ -0,0 +1,46 @@
+# string
+Horlali = "pretty"
+print(type(Horlali))
+
+# integer
+Table_number = 67
+print(type(Table_number))
+
+# decimal
+Point = 45.34
+print(type(Point))
+
+# boolean
+Freedom = True
+print(type(Freedom))
+
+
+# Operators and Operands
+
+Hour = 12
+Minutes = 30
+
+
+print(20+32)
+
+print(Hour-1)
+ 
+print(Hour*60+Minutes)
+
+print(Minutes/60)
+
+print(5**2)
+
+print((5+9)*(15-7))
+
+#Order of Operations (PEMDAS)
+
+#String operations
+First = 'throat'
+second = "warbler" 
+print(First + ' ' + second)
+
+#Operations on stings
+print(First * 3)
+print(second * 5)
+
